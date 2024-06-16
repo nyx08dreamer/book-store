7ad541f0b0b5e6d5e5fd4dfa6f3a0a4d2186bb22
nyx08dreamer <nikirv0808@gmail.com>
2024-06-16 11:17:42 -0400

    actualizacion de navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Poppins } from "next/font/google";
 import "./globals.css";
+import NavBar from "./components/nav/NavBar";
+import Footer from "./components/footer/Footer";
 
 const poppins = Poppins({ subsets: ["latin"], weight: ['400', '700'] });
 
@@ -14,13 +16,15 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
+  return ( 
     <html lang="en">
-      <body className={poppins.className}>
-        
-        <main>
-          {children}
-        </main>
+      <body className={`${poppins.className} text-slate-700`}>
+
+        <div className="flex flex-col min-h-screen">
+          <NavBar></NavBar>
+          <main className="flex-grow">{children}</main>
+          <Footer></Footer>
+        </div>
         
         </body>
     </html>
