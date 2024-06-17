8bb5ca16fba300aabab1c4cb3f9552d5c4153769
nyx08dreamer <nikirv0808@gmail.com>
2024-06-16 20:14:45 -0400

    creacion de modelo-product cards

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,21 @@
 import Image from "next/image";
 import Container from "./components/Container";
 import HomeBanner from "./components/HomeBanner";
+import { products } from "@/utils/products";
+import { truncateText } from "@/utils/truncateText";
+import ProductCard from "./components/products/ProductCard";
 
 export default function Home() {
   return (<div>
       <Container>
         <div className="p-8">
           <HomeBanner></HomeBanner>
+        </div>
+        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 
+        gap-8">
+          {products.map((product: any) =>{ 
+            return <ProductCard data={product}/>;
+            })}
         </div>
       </Container>
     </div>
