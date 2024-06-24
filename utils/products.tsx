ccbb86e48dab0602b632180747a93c00ce60352c
ByDead75 <stefanoparra15@gmail.com>
2024-06-24 17:35:31 -0400

    Inicio de todo el menu de libros

--- utils/products.tsx
+++ utils/products.tsx
@@ -2,98 +2,24 @@
 
 export const products = [
     {
-      id: "64a654593e91b8e73a351e9b",
-      name: "iphone 14",
-      description: "Short description",
-      price: 2999,
-      brand: "apple",
-      category: "Phone",
-      inStock: true,
-      images: [
-        {
-          color: "White",
-          colorCode: "#FFFFFF",
-          image:
-            "https://m.media-amazon.com/images/I/71p-tHQ0u1L._AC_SX679_.jpg",
-        },
-        {
-          color: "Gray",
-          colorCode: "#808080",
-          image:
-            "https://m.media-amazon.com/images/I/417tEj3iJ8L._AC_.jpg",
-        },
-      ],
-      reviews: [],
-    },
-    {
-      id: "64a4ebe300900d44bb50628a",
-      name: "Logitech MX Keys Advanced Wireless Illuminated Keyboard, Tactile Responsive Typing, Backlighting, Bluetooth, USB-C, Apple macOS, Microsoft Windows, Linux, iOS, Android, Metal Build (Black)",
-      description:
-        "PERFECT STROKE KEYS - Spherically-dished keys match the shape of your fingertips, offering satisfying feedback with every tap\nCOMFORT AND STABILITY - Type with confidence on a keyboard crafted for comfort, stability, and precision",
-      price: 102.99,
-      brand: "logitech",
-      category: "Accesories",
-      inStock: true,
-      images: [
-        {
-          color: "Black",
-          colorCode: "#000000",
-          image:
-            "https://m.media-amazon.com/images/I/71gOLg2-kqL.__AC_SX300_SY300_QL70_FMwebp_.jpg",
-        },
-      ],
-      reviews: [
-        {
-          id: "64a65a6158b470c6e06959ee",
-          userId: "6475af156bad4917456e6e1e",
-          productId: "64a4ebe300900d44bb50628a",
-          rating: 5,
-          comment: "good",
-          createdDate: "2023-07-06T06:08:33.067Z",
-          user: {
-            id: "6475af156bad4917456e6e1e",
-            name: "Charles",
-            email: "example@gmail.com",
-            emailVerified: null,
-            image:
-              "https://lh3.googleusercontent.com/a/AAcHTteOiCtILLBWiAoolIW9PJH-r5825pBDl824_8LD=s96-c",
-            hashedPassword: null,
-            createdAt: "2023-05-30T08:08:53.979Z",
-            updatedAt: "2023-05-30T08:08:53.979Z",
-            role: "ADMIN",
-          },
-        },
-      ],
-    },
-    {
-      id: "648437b38c44d52b9542e340",
-      name: "Apple iPhone 13, 64GB",
-      description:
-        'The product is refurbished, fully functional, and in excellent condition. Backed by the 90-day E~Shop Renewed Guarantee.\n- This pre-owned product has been professionally inspected, tested and cleaned by Amazon qualified vendors. It is not certified by Apple.\n- This product is in "Excellent condition". The screen and body show no signs of cosmetic damage visible from 12 inches away.\n- This product will have a battery that exceeds 80% capacity relative to new.\n- Accessories may not be original, but will be compatible and fully functional. Product may come in generic box.\n- Product will come with a SIM removal tool, a charger and a charging cable. Headphone and SIM card are not included.\n- This product is eligible for a replacement or refund within 90-day of receipt if it does not work as expected.\n- Refurbished phones are not guaranteed to be waterproof.',
+      id: "888437b38c44d52b9542e340",
+      name: "Harry Potter y El Cáliz de Fuego",
+      description: 
+        "A lo largo de las tres novelas anteriores pertenecientes a la saga de Harry Potter, el protagonista, Harry Potter lucha con las dificultades acarreadas por su adolescencia y por el hecho de ser un mago famoso. Cuando Harry era un niño pequeño, Voldemort, el mago tenebroso más poderoso de la historia, había asesinado a los padres de Harry, pero luego se había desvanecido misteriosamente después de que su maldición asesina en contra de Harry rebotase. El hecho había causado que Harry adquiriera fama inmediata y que fuese colocado bajo los cuidados de sus tíos muggles, Petunia y Vernon, quienes a su vez tenían un hijo llamado Dudley Dursley. Harry descubre que es un mago a los once años de edad e ingresa en el Colegio Hogwarts de Magia y Hechicería. Allí, crea amistad con Ron Weasley y Hermione Granger y se enfrenta con lord Voldemort, quien en varias ocasiones intenta regresar al poder. En el primer año de Harry en la escuela, debe proteger la Piedra Filosofal para evitar que Voldemort y uno de sus fieles vasallos, un profesor de Hogwarts, la obtuviesen. Después de regresar a la escuela, pasadas las vacaciones de verano, los estudiantes de Hogwarts sufren ataques debido a la apertura de la legendaria Cámara de los Secretos. Harry termina con los ataques cuando mata a un basilisco y frustra un nuevo intento de lord Voldemort para recuperar su fortaleza. Al año siguiente, Harry descubre que era el principal objetivo de un asesino prófugo llamado Sirius Black. Pese a las extremas medidas de seguridad que se aplican en Hogwarts, Harry se encuentra con Black al final de su tercer curso, y descubre que en realidad éste es inocente y es su padrino. También descubre que Peter Pettigrew, el amigo de Sirius y James Potter, había sido el que había traicionado a sus padres y entregado a Voldemort.",
       price: 40,
-      brand: "Apple",
-      category: "Phone",
+      brand: "J.k. Rowling",
+      category: "Fantasía",
       inStock: true,
       images: [
         {
-          color: "Black",
-          colorCode: "#000000",
           image:
-            "https://m.media-amazon.com/images/I/61g+McQpg7L._AC_SX679_.jpg",
-        },
-        {
-          color: "Blue",
-          colorCode: " #0000FF",
-          image:
-            "https://m.media-amazon.com/images/I/713Om9vCHUL._AC_SX679_.jpg",
-        },
-        {
-          color: "Red",
-          colorCode: "#FF0000",
-          image:
-            "https://m.media-amazon.com/images/I/61thdjmfHcL.__AC_SX300_SY300_QL70_FMwebp_.jpg",
+            "/img/Harry Potter y El Cáliz de Fuego.png",
         },
       ],
+      //NO SE TOCA
+      //NO SE TOCA
+      //NO SE TOCA
+      //NO SE TOCA
       reviews: [
         {
           id: "6499b4887402b0efd394d8f3",
@@ -139,54 +65,10 @@ export const products = [
       ],
     },
     {
-      id: "64a4e9e77e7299078334019f",
-      name: "Logitech MX Master 2S Wireless Mouse – Use on Any Surface, Hyper-Fast Scrolling, Ergonomic Shape, Rechargeable, Control Upto 3 Apple Mac and Windows Computers, Graphite",
-      description:
-        "Cross computer control: Game changing capacity to navigate seamlessly on 3 computers, and copy paste text, images, and files from 1 to the other using Logitech flow\nDual connectivity: Use with upto 3 Windows or Mac computers via included Unifying receiver or Bluetooth Smart wireless technology. Gesture button- Yes",
-      price: 70,
-      brand: "logitech",
-      category: "Accesories",
-      inStock: true,
-      images: [
-        {
-          color: "Graphite",
-          colorCode: " #383838",
-          image:
-            "https://m.media-amazon.com/images/I/61ni3t1ryQL.__AC_SX300_SY300_QL70_FMwebp_.jpg",
-        },
-      ],
-      reviews: [],
-    },
-    {
-      id: "649d775128b6744f0f497040",
-      name: 'Smart Watch(Answer/Make Call), 1.85" Smartwatch for Men Women IP68 Waterproof, 100+ Sport Modes, Fitness Activity Tracker, Heart Rate Sleep Monitor, Pedometer, Smart Watches for Android iOS, 2023',
-      description:
-        'Bluetooth Call and Message Reminder: The smart watch is equipped with HD speaker, after connecting to your phone via Bluetooth, you can directly use the smartwatches to answer or make calls, read messages, store contacts, view call history. The smartwatch can set up more message notifications in "GloryFit" APP. You will never miss any calls and messages during meetings, workout and riding.',
-      price: 50,
-      brand: "Nerunsa",
-      category: "Watch",
-      inStock: true,
-      images: [
-        {
-          color: "Black",
-          colorCode: "#000000",
-          image:
-            "https://m.media-amazon.com/images/I/71s4mjiit3L.__AC_SX300_SY300_QL70_FMwebp_.jpg",
-        },
-        {
-          color: "Silver",
-          colorCode: "#C0C0C0",
-          image:
-            "https://m.media-amazon.com/images/I/71zbWSRMaYL.__AC_SX300_SY300_QL70_FMwebp_.jpg",
-        },
-      ],
-      reviews: [],
-    },
-    {
       id: "888437b38c44d52b9542e340",
-      name: "Harry Potter y El Caliz de Fuego",
-      description:
-        'Libro de Magia.',
+      name: "Harry Potter y el Misterio del Príncipe",
+      description: 
+        "A lo largo de las tres novelas anteriores pertenecientes a la saga de Harry Potter, el protagonista, Harry Potter lucha con las dificultades acarreadas por su adolescencia y por el hecho de ser un mago famoso. Cuando Harry era un niño pequeño, Voldemort, el mago tenebroso más poderoso de la historia, había asesinado a los padres de Harry, pero luego se había desvanecido misteriosamente después de que su maldición asesina en contra de Harry rebotase. El hecho había causado que Harry adquiriera fama inmediata y que fuese colocado bajo los cuidados de sus tíos muggles, Petunia y Vernon, quienes a su vez tenían un hijo llamado Dudley Dursley. Harry descubre que es un mago a los once años de edad e ingresa en el Colegio Hogwarts de Magia y Hechicería. Allí, crea amistad con Ron Weasley y Hermione Granger y se enfrenta con lord Voldemort, quien en varias ocasiones intenta regresar al poder. En el primer año de Harry en la escuela, debe proteger la Piedra Filosofal para evitar que Voldemort y uno de sus fieles vasallos, un profesor de Hogwarts, la obtuviesen. Después de regresar a la escuela, pasadas las vacaciones de verano, los estudiantes de Hogwarts sufren ataques debido a la apertura de la legendaria Cámara de los Secretos. Harry termina con los ataques cuando mata a un basilisco y frustra un nuevo intento de lord Voldemort para recuperar su fortaleza. Al año siguiente, Harry descubre que era el principal objetivo de un asesino prófugo llamado Sirius Black. Pese a las extremas medidas de seguridad que se aplican en Hogwarts, Harry se encuentra con Black al final de su tercer curso, y descubre que en realidad éste es inocente y es su padrino. También descubre que Peter Pettigrew, el amigo de Sirius y James Potter, había sido el que había traicionado a sus padres y entregado a Voldemort.",
       price: 40,
       brand: "J.k. Rowling",
       category: "Fantasía",
@@ -194,9 +76,13 @@ export const products = [
       images: [
         {
           image:
-            "/banner-image.png",
+            "/img/Harry Potter y el Misterio del Príncipe.png",
         },
       ],
+      //NO SE TOCA
+      //NO SE TOCA
+      //NO SE TOCA
+      //NO SE TOCA
       reviews: [
         {
           id: "6499b4887402b0efd394d8f3",
